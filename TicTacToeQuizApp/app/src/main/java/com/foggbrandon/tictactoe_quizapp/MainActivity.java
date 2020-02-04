0a435dd7b4cb7d2aebd10a7bf354fb2531fcba5f
Brandon Fogg <2020bfogg@gmail.com>
2020-02-04 10:17:14 -0500

    Added functionality to all buttons

--- TicTacToeQuizApp/app/src/main/java/com/foggbrandon/tictactoe_quizapp/MainActivity.java
+++ TicTacToeQuizApp/app/src/main/java/com/foggbrandon/tictactoe_quizapp/MainActivity.java
@@ -59,5 +59,164 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        topB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed top button");
+                topB.setText(player);
+                if(player.equals("X")) {
+                    topB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    topB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
+
+        botB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed bottom button");
+                botB.setText(player);
+                if(player.equals("X")) {
+                    botB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    botB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
+
+        lB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed left button");
+                lB.setText(player);
+                if(player.equals("X")) {
+                    lB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    lB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
+
+        rB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed right button");
+                rB.setText(player);
+                if(player.equals("X")) {
+                    rB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    rB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
+
+        toplB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed top left button");
+                toplB.setText(player);
+                if(player.equals("X")) {
+                    toplB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    toplB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
+
+        toprB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed top right button");
+                toprB.setText(player);
+                if(player.equals("X")) {
+                    toprB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    toprB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
+
+        botlB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed bottom left button");
+                botlB.setText(player);
+                if(player.equals("X")) {
+                    botlB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    botlB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
+
+        botrB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed bottom right button");
+                botrB.setText(player);
+                if(player.equals("X")) {
+                    botrB.setTextColor(0xFFFF0000);
+                    player = "O";
+                    text.setText("O's Turn");
+                    text.setTextColor(0xFF0000FF);
+                }
+                else {
+                    botrB.setTextColor(0xFF0000FF);
+                    player = "X";
+                    text.setText("X's Turn");
+                    text.setTextColor(0xFFFF0000);
+                }
+            }
+        });
     }
 }
