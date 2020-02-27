422f026c1e451ba77da6e360f1a1df96e8fda2f9
Brandon Fogg <2020bfogg@gmail.com>
2020-02-27 10:14:38 -0500

    Started adding single player mode

--- TicTacToeQuizApp/app/src/main/java/com/foggbrandon/tictactoe_quizapp/MainActivity.java
+++ TicTacToeQuizApp/app/src/main/java/com/foggbrandon/tictactoe_quizapp/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     TextView text;
     Button singlePlayer;
     Button multiPlayer;
+    Button chooseX;
+    Button chooseO;
     Button backB;
     Button midB;
     Button topB;
@@ -73,12 +75,357 @@ public class MainActivity extends AppCompatActivity {
         botlB.setClickable(false);
     }
 
-    public void startSinglePlayerGame() {
+    public void AI_take_turn(String other_player) {
+        String AI_player = "";
+        if(other_player.equals("X")) {
+            AI_player = "O";
+        }
+        else {
+            AI_player = "X";
+        }
+    }
 
+    public void startSinglePlayerGame() {
+        setContentView(R.layout.single_player_screen);
+
+        player = "";
+        text = findViewById(R.id.text);
+        backB = findViewById(R.id.backB);
+        chooseX = findViewById((R.id.chooseX));
+        chooseO = findViewById((R.id.chooseO));
+        midB = findViewById(R.id.midB);
+        topB = findViewById(R.id.topB);
+        toplB = findViewById(R.id.toplB);
+        toprB = findViewById(R.id.toprB);
+        lB = findViewById(R.id.lB);
+        rB = findViewById(R.id.rB);
+        botB = findViewById(R.id.botB);
+        botlB = findViewById(R.id.botlB);
+        botrB = findViewById(R.id.botrB);
+        resetB = findViewById(R.id.resetB);
+
+        midB.setBackgroundColor(0x10909090);
+        topB.setBackgroundColor(0x10909090);
+        botB.setBackgroundColor(0x10909090);
+        rB.setBackgroundColor(0x10909090);
+        lB.setBackgroundColor(0x10909090);
+        toprB.setBackgroundColor(0x10909090);
+        toplB.setBackgroundColor(0x10909090);
+        botrB.setBackgroundColor(0x10909090);
+        botlB.setBackgroundColor(0x10909090);
+
+        midB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed middle button");
+                if (!midB.getText().equals("X") && !midB.getText().equals("O")) {
+                    midB.setText(player);
+                    if (player.equals("X")) {
+                        midB.setTextColor(0xFFFF0000);
+                        midB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        midB.setTextColor(0xFF0000FF);
+                        midB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        topB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed top button");
+                if(!topB.getText().equals("X") && !topB.getText().equals("O")) {
+                    topB.setText(player);
+                    if (player.equals("X")) {
+                        topB.setTextColor(0xFFFF0000);
+                        topB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        topB.setTextColor(0xFF0000FF);
+                        topB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        botB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed bottom button");
+                if(!botB.getText().equals("X") && !botB.getText().equals("O")) {
+                    botB.setText(player);
+                    if (player.equals("X")) {
+                        botB.setTextColor(0xFFFF0000);
+                        botB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        botB.setTextColor(0xFF0000FF);
+                        botB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        lB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed left button");
+                if(!lB.getText().equals("X") && !lB.getText().equals("O")) {
+                    lB.setText(player);
+                    if (player.equals("X")) {
+                        lB.setTextColor(0xFFFF0000);
+                        lB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        lB.setTextColor(0xFF0000FF);
+                        lB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        rB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed right button");
+                if(!rB.getText().equals("X") && !rB.getText().equals("O")) {
+                    rB.setText(player);
+                    if(player.equals("X")) {
+                        rB.setTextColor(0xFFFF0000);
+                        rB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    }
+                    else {
+                        rB.setTextColor(0xFF0000FF);
+                        rB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        toplB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed top left button");
+                if(!toplB.getText().equals("X") && !toplB.getText().equals("O")) {
+                    toplB.setText(player);
+                    if (player.equals("X")) {
+                        toplB.setTextColor(0xFFFF0000);
+                        toplB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        toplB.setTextColor(0xFF0000FF);
+                        toplB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        toprB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed top right button");
+                if(!toprB.getText().equals("X") && !toprB.getText().equals("O")) {
+                    toprB.setText(player);
+                    if (player.equals("X")) {
+                        toprB.setTextColor(0xFFFF0000);
+                        toprB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        toprB.setTextColor(0xFF0000FF);
+                        toprB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        botlB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed bottom left button");
+                if(!botlB.getText().equals("X") && !botlB.getText().equals("O")) {
+                    botlB.setText(player);
+                    if (player.equals("X")) {
+                        botlB.setTextColor(0xFFFF0000);
+                        botlB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        botlB.setTextColor(0xFF0000FF);
+                        botlB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        botrB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", player + " pressed bottom right button");
+                if(!botrB.getText().equals("X") && !botrB.getText().equals("O")) {
+                    botrB.setText(player);
+                    if (player.equals("X")) {
+                        botrB.setTextColor(0xFFFF0000);
+                        botrB.setBackgroundColor(0x20FF0000);
+                        AI_take_turn(player);
+                    } else {
+                        botrB.setTextColor(0xFF0000FF);
+                        botrB.setBackgroundColor(0x200000FF);
+                        AI_take_turn(player);
+                    }
+                    String win = isWinner();
+                    if(!win.equals("")){
+                        endGame(win);
+                    }
+                }
+            }
+        });
+
+        backB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", "Pressed back button");
+                startScreen();
+            }
+        });
+
+        chooseO.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", "Player chose O");
+                player = "O";
+                chooseO.setVisibility(View.INVISIBLE);
+                chooseX.setVisibility(View.INVISIBLE);
+                chooseO.setClickable(false);
+                chooseX.setClickable(false);
+                toplB.setClickable(true);
+                topB.setClickable(true);
+                toprB.setClickable(true);
+                lB.setClickable(true);
+                midB.setClickable(true);
+                rB.setClickable(true);
+                botrB.setClickable(true);
+                botB.setClickable(true);
+                botlB.setClickable(true);
+                AI_take_turn(player);
+            }
+        });
+
+        chooseX.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", "Player chose X");
+                player = "X";
+                chooseO.setVisibility(View.INVISIBLE);
+                chooseX.setVisibility(View.INVISIBLE);
+                chooseO.setClickable(false);
+                chooseX.setClickable(false);
+                toplB.setClickable(true);
+                topB.setClickable(true);
+                toprB.setClickable(true);
+                lB.setClickable(true);
+                midB.setClickable(true);
+                rB.setClickable(true);
+                botrB.setClickable(true);
+                botB.setClickable(true);
+                botlB.setClickable(true);
+            }
+        });
+
+        resetB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("button", "Pressed reset button");
+                midB.setText("");
+                topB.setText("");
+                botB.setText("");
+                lB.setText("");
+                rB.setText("");
+                toplB.setText("");
+                botlB.setText("");
+                toprB.setText("");
+                botrB.setText("");
+                midB.setBackgroundColor(0x10909090);
+                topB.setBackgroundColor(0x10909090);
+                botB.setBackgroundColor(0x10909090);
+                rB.setBackgroundColor(0x10909090);
+                lB.setBackgroundColor(0x10909090);
+                toprB.setBackgroundColor(0x10909090);
+                toplB.setBackgroundColor(0x10909090);
+                botrB.setBackgroundColor(0x10909090);
+                botlB.setBackgroundColor(0x10909090);
+                toplB.setClickable(false);
+                topB.setClickable(false);
+                toprB.setClickable(false);
+                lB.setClickable(false);
+                midB.setClickable(false);
+                rB.setClickable(false);
+                botrB.setClickable(false);
+                botB.setClickable(false);
+                botlB.setClickable(false);
+                player = "";
+                chooseO.setVisibility(View.VISIBLE);
+                chooseX.setVisibility(View.VISIBLE);
+                chooseO.setClickable(true);
+                chooseX.setClickable(true);
+                text.setText("");
+            }
+        });
+
+        toplB.setClickable(false);
+        topB.setClickable(false);
+        toprB.setClickable(false);
+        lB.setClickable(false);
+        midB.setClickable(false);
+        rB.setClickable(false);
+        botrB.setClickable(false);
+        botB.setClickable(false);
+        botlB.setClickable(false);
     }
 
     public void startMultiPlayerGame() {
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.multi_player_screen);
         player = "X";
         text = findViewById(R.id.text);
         backB = findViewById(R.id.backB);
